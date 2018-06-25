5542a2c7a2182b36d52af7803e451d8bb3f20af0
SteveSWD <37374151+SteveSWD@users.noreply.github.com>
2018-06-25 18:04:27 -0400

    Update urls.py

--- adpapi/api/urls.py
+++ adpapi/api/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
         path('tasks/', views.tasks, name='tasks'),
-        path('update/<int:task_id>/', views.update, name='result'),
+        path('update/', views.update, name='result'),
         path('new/', views.new, name='new')
 ]
